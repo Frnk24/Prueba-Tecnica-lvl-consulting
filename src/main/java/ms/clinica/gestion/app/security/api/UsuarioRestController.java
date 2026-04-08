285dd4f6401c7511ab4964814f4f543ad2691f72
Frnk24 <franko.1998.11.25@gmail.com>
2026-04-07 22:06:26 -0500

    Documentos - prueba tecnica y archivo yml para la DB

--- src/main/java/ms/clinica/gestion/app/security/api/UsuarioRestController.java
+++ src/main/java/ms/clinica/gestion/app/security/api/UsuarioRestController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/usuario")
 @RequiredArgsConstructor
 public class UsuarioRestController {
     private final UsuarioFacade usuarioFacade;
